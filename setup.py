34a8b11f49982612edcd1f24fcce3b64bbbac35a
erwinmintiens <erwin.mintiens@gmail.com>
2021-06-01 16:15:01 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='signinghub-api',
     version='0.1.0',
     description='Package with default calls for the SigningHub API',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     author='Erwin Mintiens',
     author_email='erwin.mintiens@gmail.com',
     packages=find_packages(exclude=['docs', 'tests*']),
