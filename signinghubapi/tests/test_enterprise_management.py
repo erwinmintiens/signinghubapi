95e180ac1392ffbd5e38b963adc940f45017a6e6
erwinmintiens <erwin.mintiens@protonmail.com>
2022-10-12 22:26:28 +0200

    Cleanup

--- signinghubapi/tests/test_enterprise_management.py
+++ signinghubapi/tests/test_enterprise_management.py
@@ -9,17 +9,20 @@ from .utils import MockResponse
 
 
 @pytest.mark.parametrize(
-    "version, result", [("7.0.0.0", 3), ("7.7.9.0", 4), ("8.2.4", 4)]
+    "initial_api_version, signinghub_version, result_api_version",
+    [(4, "7.0.0.0", 3), (3, "7.7.9.0", 4), (3, "8.2.4", 4)],
 )
-def test_about_signinghub_set_api_version(version, result):
-    conn = Connection(url="https://testurl.com")
+def test_about_signinghub_set_api_version(
+    initial_api_version, signinghub_version, result_api_version
+):
+    conn = Connection(url="https://testurl.com", api_version=initial_api_version)
     with patch("signinghubapi.signinghubapi.requests.get") as mock_get:
         mock_get.return_value = MockResponse(
             status_code=200,
             text=json.dumps(
                 {
                     "installation_name": "SigningHub",
-                    "version": version,
+                    "version": signinghub_version,
                     "build": "6100.6100.280415.22852",
                     "patents": {"us_patent_no": "7,360,079"},
                     "copyright": "Ascertia Limited. All rights reserved.",
@@ -29,19 +32,22 @@ def test_about_signinghub_set_api_version(version, result):
 
         conn.about_signinghub(set_api_version=True)
 
+    assert conn.api_version == result_api_version
+
 
 @pytest.mark.parametrize(
-    "version, result", [("7.7.9.0", 3), ("8.2.4", 3), ("7.7.6", 4)]
+    "signinghub_version, api_version",
+    [("7.7.9.0", 3), ("8.2.4", 3), ("7.7.6", 4)],
 )
-def test_about_signinghub_not_set_api_version(version, result):
-    conn = Connection(url="https://testurl.com", api_version=result)
+def test_about_signinghub_not_set_api_version(signinghub_version, api_version):
+    conn = Connection(url="https://testurl.com", api_version=api_version)
     with patch("signinghubapi.signinghubapi.requests.get") as mock_get:
         mock_get.return_value = MockResponse(
             status_code=200,
             text=json.dumps(
                 {
                     "installation_name": "SigningHub",
-                    "version": version,
+                    "version": signinghub_version,
                     "build": "6100.6100.280415.22852",
                     "patents": {"us_patent_no": "7,360,079"},
                     "copyright": "Ascertia Limited. All rights reserved.",
@@ -51,4 +57,4 @@ def test_about_signinghub_not_set_api_version(version, result):
 
         conn.about_signinghub(set_api_version=False)
 
-    assert conn.api_version == result
+    assert conn.api_version == api_version
