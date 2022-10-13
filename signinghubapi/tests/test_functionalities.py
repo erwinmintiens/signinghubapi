ed4e6f5e814867851c12df70ce540b09bf89d1db
erwinmintiens <erwin.mintiens@protonmail.com>
2022-10-13 15:09:56 +0200

    Added full url testing with new api port numbers

--- signinghubapi/tests/test_functionalities.py
+++ signinghubapi/tests/test_functionalities.py
@@ -2,7 +2,6 @@ import json
 import unittest
 
 import requests
-
 from signinghubapi.signinghubapi import Connection
 from signinghubapi.utils import GET_HEADERS_NO_AUTH
 
@@ -17,3 +16,22 @@ class TestRaiseIfWrongValue(unittest.TestCase):
         conn = Connection(url="https://test.com")
         with self.assertRaises(ValueError):
             conn.api_version = 5
+
+
+def test_url_ending_with_slash():
+    conn = Connection(url="https://test.com/")
+    assert conn.full_url == "https://test.com"
+
+    conn.url = "https://test2.com/"
+    assert conn.full_url == "https://test2.com"
+
+
+def test_api_port():
+    conn = Connection(url="https://test.com/", api_port=433)
+    assert conn.full_url == "https://test.com:433"
+    conn.api_port = None
+    assert conn.full_url == "https://test.com"
+
+    conn = Connection(url="https://test.com/", api_port=None)
+    conn.api_port = 433
+    assert conn.full_url == "https://test.com:433"
