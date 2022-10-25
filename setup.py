23ba32e6af3e5e99c12416827275efacd62eb2f1
erwinmintiens <erwin.mintiens@protonmail.com>
2022-10-25 16:04:23 +0200

    Updating package to new version

--- setup.py
+++ setup.py
@@ -5,25 +5,25 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 setup(
-    name='signinghubapi',
-    version='0.1.3',
-    description='Package with default calls for the SigningHub API',
+    name="signinghubapi",
+    version="0.1.4",
+    description="Package with default calls for the SigningHub API",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/erwinmintiens/signinghub-api',
-    author='Erwin Mintiens',
-    author_email='erwin.mintiens@gmail.com',
-    keywords=['SigningHub', 'API'],
-    packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=['requests'],
-    license_files=('LICENSE',),
+    long_description_content_type="text/markdown",
+    url="https://github.com/erwinmintiens/signinghub-api",
+    author="Erwin Mintiens",
+    author_email="erwin.mintiens@protonmail.com",
+    keywords=["SigningHub", "API"],
+    packages=find_packages(exclude=["docs", "tests*"]),
+    install_requires=["requests"],
+    license_files=("LICENSE",),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-        "License :: OSI Approved :: MIT License"
-    ]
+        "License :: OSI Approved :: MIT License",
+    ],
 )
